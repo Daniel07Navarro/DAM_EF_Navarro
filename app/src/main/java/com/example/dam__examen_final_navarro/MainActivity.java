e5d21d069261f7f4a8fc876cc6a814735dfca316
Daniel07Navarro <daniel7navarro@gmail.com>
2024-07-17 09:00:28 -0500

    Primer commit - Login

---
+++ app/src/main/java/com/example/dam__examen_final_navarro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dam__examen_final_navarro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
